027ba0bec84aa5e824acbee76437109875b48fea
Julie Nisbet <jmnisbet@gmail.com>
2022-10-11 11:19:29 -0700

    add zip search

---
+++ functions/yelp/index.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+require('dotenv').config({ path: `.env.development.local` });
+
+exports.handler = async (event) => {
+  const zip = event.queryStringParameters.zip;
+  try {
+    const resp = await fetch(
+      `https://api.yelp.com/v3/businesses/search?categories=restaurants&location=${zip}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+        },
+      }
+    );
+    const data = await resp.json();
+    console.log(data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.businesses),
+    };
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
+};
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [quotes, setQuotes] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
+  const [zip, setZip] = useState('93117');
   useEffect(() => {
     const fetchData = async () => {
       const resp = await fetch('/.netlify/functions/cat-endpoint');
@@ -11,10 +13,48 @@ function App() {
     };
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const fetchYelp = async () => {
+      const resp = await fetch(`/.netlify/functions/yelp?zip=${zip}`);
+      const data = await resp.json();
+      setRestaurants(data);
+    };
+    fetchYelp();
+  }, []);
+
+  const searchRestaurants = async () => {
+    console.log(zip);
+    const resp = await fetch(`/.netlify/functions/yelp?zip=${zip}`);
+    const data = await resp.json();
+    setRestaurants(data);
+    // call the API with the zip
+    // set the restaurants in state
+  };
+
+  // This will fail because of CORS
+
+  // useEffect(() => {
+  //   const fetchYelp = async () => {
+  //     const resp = await fetch(
+  //       `https://api.yelp.com/v3/businesses/search?categories=restaurants&location=93117`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+  //         },
+  //       }
+  //     );
+  //     const data = await resp.json();
+  //     setRestaurants(data);
+  //   };
+  //   fetchYelp();
+  // }, []);
   return (
     <div className="App">
-      {quotes.map((quote, idx) => (
-        <div key={idx}>{quote.quote}</div>
+      <input value={zip} onChange={(e) => setZip(e.target.value)} />
+      <button onClick={searchRestaurants}>Search</button>
+      {restaurants.map((restaurant) => (
+        <div key={restaurant.id}>{restaurant.name}</div>
       ))}
     </div>
   );
